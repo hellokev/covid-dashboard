2e9032891d0530ef92ea4677aa46145fc2469ce6
Kevin Loi <2kevinloi8@gmail.com>
2023-12-05 23:31:50 -0800

    incomplete routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import CovidTable from './components/CovidTable'
+import IndexPage from './pages/index.js'
 
 function App() {
   return (
     <>
       <h1>COVID-19 Dashboard</h1>
       <CovidTable />
+      <IndexPage />
     </>
   )
 }
