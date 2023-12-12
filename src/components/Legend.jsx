feff58dc6fe0c01854813cbd52bd9d38dfd8b542
hellokev <2kevinloi8@gmail.com>
2023-12-12 01:44:35 -0800

    updated legend

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -3,13 +3,14 @@ import '../assets/stylesheets/components/legend.css';
 const Legend = () => {
     return (
         <div className="legend">
-            <div style={{ "--color": '#67001f' }}>6000+</div>
-            <div style={{ "--color": '#980043' }}>5001 - 6000</div>
-            <div style={{ "--color": '#e1256' }}>1001 - 5000</div>
-            <div style={{ "--color": '#e7298a' }}>501 - 1000</div>
-            <div style={{ "--color": '#d4b9da'}}>100 - 500</div>
-            <div style={{ "--color": '#f1eef6' }}>0-99</div>
+            <div style={{ "--color": '#ff00ff' }}>1000000+</div>
+            <div style={{ "--color": '#d400d4' }}>800001 - 1000000</div>
+            <div style={{ "--color": '#b300b3' }}>600001 - 800000</div>
+            <div style={{ "--color": '#990099' }}>400001 - 600000</div>
+            <div style={{ "--color": '#7a007a' }}>200001 - 400000</div>
+            <div style={{ "--color": '#4d004d' }}>0 - 200000</div>
         </div>
     );
 }
+
 export default Legend;
