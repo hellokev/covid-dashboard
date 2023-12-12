013fa8173b95b708b0c23af990c320bacaa9537e
hellokev <2kevinloi8@gmail.com>
2023-12-12 00:31:25 -0800

    Updated tables

--- src/components/CovidTable.jsx
+++ src/components/CovidTable.jsx
@@ -20,52 +20,61 @@ function CovidTable() {
     }, {})
 
     return (
-        <TableContainer component={Paper}>
-            <h2>United States Country Wide Covid Data</h2>
-            <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Confirmed</TableCell>
-                        <TableCell>Active</TableCell>
-                        <TableCell>Recovered</TableCell>
-                        <TableCell>Deaths</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    <TableRow
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                        <TableCell>{latestNatData.totalTestResults}</TableCell>
-                        <TableCell>{latestNatData.positive}</TableCell>
-                        <TableCell>dummy data</TableCell>
-                        <TableCell>{latestNatData.death}</TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-            <h2>Per State Covid Data</h2>
-            <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>State</TableCell>
-                        <TableCell>Active</TableCell>
-                        <TableCell>Recovered</TableCell>
-                        <TableCell>Deaths</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {Object.values(latestStateDate).map((stateInfo) => (
-                        <TableRow key={stateInfo}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell>{stateInfo.state}</TableCell>
-                            <TableCell>{stateInfo.positive}</TableCell>
-                            <TableCell>dummy data</TableCell>
-                            <TableCell>{stateInfo.death}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Paper sx={{ width: '50%' }}>
+                <TableContainer component={Paper} sx={{ maxHeight: 440 }}>
+                    <h2>United States Country Wide Covid Data</h2>
+                    <Table sx={{ maxWidth: 650 }} aria-label='simple table'>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Confirmed</TableCell>
+                                <TableCell>Active</TableCell>
+                                <TableCell>Recovered</TableCell>
+                                <TableCell>Deaths</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell>{latestNatData.totalTestResults}</TableCell>
+                                <TableCell>{latestNatData.positive}</TableCell>
+                                <TableCell>dummy data</TableCell>
+                                <TableCell>{latestNatData.death}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+            
+            <Paper sx={{ width: '50%' }}>
+                <TableContainer component={Paper} sx={{ maxHeight: 440 }}>
+                    <h2>Per State Covid Data</h2>
+                    <Table sx={{ maxWidth: 650 }} aria-label='simple table'>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>State</TableCell>
+                                <TableCell>Active</TableCell>
+                                <TableCell>Recovered</TableCell>
+                                <TableCell>Deaths</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {Object.values(latestStateDate).map((stateInfo) => (
+                                <TableRow key={stateInfo}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell>{stateInfo.state}</TableCell>
+                                    <TableCell>{stateInfo.positive}</TableCell>
+                                    <TableCell>dummy data</TableCell>
+                                    <TableCell>{stateInfo.death}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+        </div>
     );
 }
 
