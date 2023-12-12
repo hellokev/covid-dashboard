9a0f0742bbcead4ecd484a2dde1b474b51f091a5
hellokev <2kevinloi8@gmail.com>
2023-12-12 00:41:37 -0800

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import './App.css'
 import CovidTable from './components/CovidTable'
 import Map from './components/Map.jsx'
 import Legend from './components/Legend.jsx';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+  
+  
   return (
     <>
-      <h1>COVID-19 Dashboard</h1>
-      <CovidTable />
-      <Map />
-      <Legend />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <h1>COVID-19 Dashboard</h1>
+        <CovidTable />
+        <Map />
+        <Legend />
+      </ThemeProvider>
     </>
   )
 }
