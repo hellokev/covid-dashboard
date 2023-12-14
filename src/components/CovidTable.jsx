84db0236109c4d0783a7fa351d287e5399972d29
hellokev <2kevinloi8@gmail.com>
2023-12-14 15:31:07 -0800

    added one more table + some styling

--- src/components/CovidTable.jsx
+++ src/components/CovidTable.jsx
@@ -19,9 +19,11 @@ function CovidTable() {
         return acc
     }, {})
 
+    const neonPurpleBoxShadow = '4px 4px 3px 0px rgba(148, 87, 235, 0.7)';
+
     return (
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Paper sx={{ width: '50%' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', padding: '5px' }}>
+            <Paper sx={{ width: '50%', margin: '5px', boxShadow: neonPurpleBoxShadow }}>
                 <TableContainer component={Paper} sx={{ maxHeight: 440 }}>
                     <h2>United States Country Wide Covid Data</h2>
                     <Table sx={{ maxWidth: 650 }} aria-label='simple table'>
@@ -46,8 +48,34 @@ function CovidTable() {
                     </Table>
                 </TableContainer>
             </Paper>
+
+            <Paper sx={{ width: '50%', margin: '5px', boxShadow: neonPurpleBoxShadow }}>
+                <TableContainer component={Paper} sx={{ maxHeight: 440 }}>
+                    <h2>ICUs and Hospitals</h2>
+                    <Table sx={{ maxWidth: 650 }} aria-label='simple table'>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Icu Cumulative</TableCell>
+                                <TableCell>Icu Currently</TableCell>
+                                <TableCell>Hospitalized Cumulative</TableCell>
+                                <TableCell>Hospitalized Currently</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        <TableRow
+                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                         >
+                                 <TableCell>{latestNatData.inIcuCumulative}</TableCell>
+                                 <TableCell>{latestNatData.inIcuCurrently}</TableCell>
+                                 <TableCell>{latestNatData.hospitalizedCumulative}</TableCell>
+                                 <TableCell>{latestNatData.hospitalizedCurrently}</TableCell>
+                             </TableRow>                            
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
             
-            <Paper sx={{ width: '50%' }}>
+            <Paper sx={{ width: '50%', margin: '5px', boxShadow: neonPurpleBoxShadow }}>
                 <TableContainer component={Paper} sx={{ maxHeight: 440 }}>
                     <h2>Per State Covid Data</h2>
                     <Table sx={{ maxWidth: 650 }} aria-label='simple table'>
@@ -74,6 +102,7 @@ function CovidTable() {
                     </Table>
                 </TableContainer>
             </Paper>
+
         </div>
     );
 }
